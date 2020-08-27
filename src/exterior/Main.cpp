78201e549bc02c92c34025f6852c9b218dc45eb9
Abel Gómez <abgolla@gmail.com>
2020-08-27 03:37:07 +0200

    Fix permanent sleep when connection fails

--- src/exterior/Main.cpp
+++ src/exterior/Main.cpp
@@ -3,37 +3,45 @@
 #include <ESP8266mDNS.h>
 
 #include "ConnectionManager.h"
-#include "exterior/Sensors.h"
 #include "exterior/ExteriorConfigManager.h"
 #include "exterior/MqttManager.h"
+#include "exterior/Sensors.h"
 
-#define WIFI_TIMEOUT 7000
+#define WIFI_TIMEOUT 15000
+#define WIFI_RETRIES 3
 
 void setup() {
-	Serial.begin(9600);
-	Serial.println("");
+    Serial.begin(9600);
+    Serial.println("");
 
-	Sensors sensors;
-	ExteriorConfigManager configManager;
-	if (sensors.configButtonPressed()) {
-		configManager.configure();
-	}
-
-	ConnectionManager connectionManager;
-	if (connectionManager.connect(WIFI_TIMEOUT) != CONNECTIONMANAGER_CONNECTED) {
-        ESP.deepSleep(0);
-	}
-
-    if (!MDNS.begin(configManager.getHostname().c_str())) {
-        Serial.println("Cannot start mDNS");
+    Sensors sensors;
+    ExteriorConfigManager configManager;
+    if (sensors.configButtonPressed()) {
+        configManager.configure();
     }
-    Serial.println("mDNS started as " + String(configManager.getHostname().c_str()));
 
-	MqttManager mqttManager(configManager.getBroker(), configManager.getBrokerPort());
-	mqttManager.publish(sensors);
+    ConnectionManager connectionManager;
+    Serial.println("Connecting to WiFi...");
+    for (int retries = WIFI_RETRIES; retries > 0; retries--) {
+        Serial.print(retries);
+        Serial.println(" retries left...");
+        int result = connectionManager.connect(WIFI_TIMEOUT);
+        if (result == CONNECTIONMANAGER_ERR_TIMEOUT) {
+            continue;
+        } else if (result == CONNECTIONMANAGER_CONNECTED) {
+            if (!MDNS.begin(configManager.getHostname().c_str())) {
+                Serial.println("Cannot start mDNS");
+            }
+            Serial.println("mDNS started as " + String(configManager.getHostname().c_str()));
 
-	Serial.printf("Now sleeping for %d minutes.\n", configManager.getUpdateTimeMinutes());
-	ESP.deepSleep(configManager.getUpdateTimeMicroSeconds());
+            MqttManager mqttManager(configManager.getBroker(), configManager.getBrokerPort());
+            mqttManager.publish(sensors);
+            break;
+        }
+    }
+
+    Serial.printf("Now sleeping for %d minutes.\n", configManager.getUpdateTimeMinutes());
+    ESP.deepSleep(configManager.getUpdateTimeMicroSeconds());
 }
 
 void loop() {
